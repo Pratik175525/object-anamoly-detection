054b7dbd35e32786d2221ea71b7b740661d63bdf
Pratik175525 <pratikbaria2562004@gmail.com>
2025-07-23 00:50:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,14 +23,14 @@ output_details = interpreter.get_output_details()
 # Streamlit UI
 st.title("Bottle Defect Detection")
 
-uploaded_file = st.file_uploader("Upload an image of a bottle", type=["jpg", "jpeg", "png", "webp"])
+uploaded_file = st.file_uploader("Upload an image of a bottle", type=["jpg", "jpeg", "png","webp"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
     # Resize and preprocess image
-    size = (224, 224)
+    size = (224, 224)  # or change based on model
     image = ImageOps.fit(image, size, method=Image.Resampling.LANCZOS)
     image_array = np.asarray(image, dtype=np.float32) / 255.0
     image_array = np.expand_dims(image_array, axis=0)
